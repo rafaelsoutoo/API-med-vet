706e9ebd579d5ba8e8a30131f7e7ce4e4392dcd7
yurip <yuripeixoto1112@gmail.com>
2024-04-11 17:20:46 -0300

    fix: adding cors support from web requests

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,10 @@
 import { app } from "./app";
 import { env } from "./env";
+import cors from '@fastify/cors'
 
+app.register(cors, {
+  origin: '*'
+});
 
 app.listen({
   host: '0.0.0.0',//seja accessível
