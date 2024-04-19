20099d01a79d78fcca0daf554a8c152f9e7ed20c
yurip <yuripeixoto1112@gmail.com>
2024-04-19 00:03:32 -0300

    feat: setting the data return of the get all consults

--- src/repositories/Prisma/prisma-consults-repository.ts
+++ src/repositories/Prisma/prisma-consults-repository.ts
@@ -15,18 +15,18 @@ export class PrismaConsultsRepository implements ConsultsRepository {
     return consult
   }
 
-  async getAllConsultsDone(page: number, numberOfItems: number) {
-    const skipItens = (page - 1) * numberOfItems
+  async getAllConsultsDone() {
+    // const skipItens = (page - 1) * numberOfItems
     
     const consults = await prisma.consult.findMany({
       where: {
         done: false
       },
       orderBy: {
-        date: 'asc'
-      },
-      take: numberOfItems,
-      skip: skipItens
+        date: 'desc'
+      }
+      // take: numberOfItems,
+      // skip: skipItens
     })
 
     return consults
