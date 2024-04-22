0e3d3ee228984e4fc457530bffaf8445476b7a08
rafaelsoutoo <rafaelsouto70017@gmail.com>
2024-04-22 15:57:14 -0300

    fix: resolver message error in get student and teacher

--- src/use-cases/getTeachers.ts
+++ src/use-cases/getTeachers.ts
@@ -1,10 +1,16 @@
 import { UsersRepository } from "@/repositories/users-repository";
+import { NoExistsUsersError } from "./errors/no-exists-users-error";
 
 export class GetAllTeachersUseCase {
   constructor(private usersRepository: UsersRepository) { }
 
   async execute(page: number, numberOfItems: number) {
     const users = await this.usersRepository.findAllTeachers(page, numberOfItems);
+
+    if (users.length === 0) {
+      throw new NoExistsUsersError
+    }
+
     return users;
   }
 }
