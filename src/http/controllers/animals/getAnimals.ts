2cc823544d2b210d454f9331ff76aca07bb2e584
yurip <yuripeixoto1112@gmail.com>
2024-05-02 17:00:48 -0300

    feat: add route to search for animals using the tutor's ID

--- src/http/controllers/animals/getAnimals.ts
+++ src/http/controllers/animals/getAnimals.ts
@@ -1,8 +1,10 @@
 import { FastifyRequest, FastifyReply } from "fastify";
 import { PrismaAnimalsRepository } from "@/repositories/Prisma/prisma-animals-repository";
-import { GetAllAnimalsUseCase } from "@/use-cases/animal/getAnimals";
+import { GetAllAnimalsUseCase, GetAnimalByTutorUseCase } from "@/use-cases/animal/getAnimals";
 import { z } from "zod";
 import { AnimalNoexists } from "@/use-cases/errors/animal-errors";
+import { makeGetByTutorAnimalUseCase } from "@/use-cases/factories/animals/make-get-by-tutor-animal";
+import { TutorNotExistsError } from "@/use-cases/errors/tutor-error";
 
 export async function getAllAnimals(request: FastifyRequest, reply: FastifyReply) {
     const getQuerySchema = z.object({
@@ -23,4 +25,28 @@ export async function getAllAnimals(request: FastifyRequest, reply: FastifyReply
             return reply.status(404).send({ message: error.message })
         }
     }
+}
+
+export async function getAnimalsByTutor(request: FastifyRequest, reply: FastifyReply) {
+    const getQuerySchema = z.object({
+        tutor_id: z.string()
+    });
+
+    const { tutor_id } = getQuerySchema.parse(request.params)
+
+    try {
+        const getusecase = makeGetByTutorAnimalUseCase()
+
+        const animals = await getusecase.execute(tutor_id)
+
+        return reply.status(200).send(animals)
+    } catch(error) {
+        if(error instanceof TutorNotExistsError){
+            return reply.status(404).send({message: error.message})
+        }
+    }
+
+
+
+    
 }
