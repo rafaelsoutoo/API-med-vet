d4111697e7cc27ea1813ac55dfacbd0c076cd750
rafaelsoutoo <rafaelsouto70017@gmail.com>
2024-05-15 17:11:37 -0300

    fix: add sequence in repositoy consult

--- src/repositories/animal-repository.ts
+++ src/repositories/animal-repository.ts
@@ -1,13 +1,13 @@
 import { Prisma, Animal } from '@prisma/client'
 
 export interface AnimalRepository {
+  sequence(): Promise<string>
   findById(id: string): Promise<Animal | null>
   findBySequence(sequence: string): Promise<Animal | null>
   createAnimal(data: Prisma.AnimalUncheckedCreateInput): Promise<Animal>
   findManyIdTutor(tutor_id: string): Promise<Animal[]>
   getAllAnimals(page: number, numberOfItems: number): Promise<Animal[]>
   findByTutor(id: string): Promise<Animal[]>
-  sequence(): Promise<string>
 
 
   // searchAnimalByNameTutor(name: string): Promise<Animal[]>
